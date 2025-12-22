b3f35fead987290c2f088105bb4f9c8122b7a421
Raflot <swann.piau@ensea.fr>
2025-12-22 19:32:42 +0100

    charcter + json

--- board.py
+++ board.py
@@ -1,4 +1,20 @@
-def board_init():
+import numpy as np
+x=30
+y=30
+
+def board_init(x,y):
     print("Board initialized")
-    BOARD = []
+    BOARD = np.zeros((2*x, 2*y))
+    for i in range(2*x):
+        for j in range(2*y):
+            if i%2==0 and j%2==0:
+                BOARD[i][j] = 1
+            if i%2==1 and j%2==1:
+                BOARD[i][j] = 1
+            if i>(3*np.sqrt(3)/2)*x*y or j>(3*np.sqrt(3)/2)*x*y:
+                BOARD[i][j] = -1
     return BOARD
+
+board = board_init(x,y)
+
+print(board)
--- characters.py
+++ characters.py
@@ -1,4 +1,14 @@
+
+
 def character_init():
     print("Character initialized")
-    CHARACTER = {}
+    
+def character_create(name, nickname, id, player, position, available_positions, last_position, spell, invocations, ultimate, missions, attack, defense, ultimate_ready, spell_ready, alive):
+    CHARACTER = {'name': name, 'nickname': nickname, 'id': id, 'player': player, 'position': position, 'available_positions': available_positions, 'last_position': last_position, 'spell': spell, 'invocations': invocations, 'ultimate': ultimate, 'missions': missions, 'attack': attack, 'defense': defense, 'ultimate_ready': ultimate_ready, 'spell_ready': spell_ready, 'alive': alive}
+    print("Character created:", CHARACTER)
+    return CHARACTER
+
+def character_update(CHARACTER, data):
+    CHARACTER.update(data)
+    print("Character updated:", CHARACTER)
     return CHARACTER
