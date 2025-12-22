baf7f6dba135d08746099cc934de041829c186f7
Raflot <swann.piau@ensea.fr>
2025-12-22 20:24:52 +0100

    board

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 x=30
 y=30
 
@@ -11,10 +12,22 @@ def board_init(x,y):
                 BOARD[i][j] = 1
             if i%2==1 and j%2==1:
                 BOARD[i][j] = 1
-            if i>(3*np.sqrt(3)/2)*x*y or j>(3*np.sqrt(3)/2)*x*y:
+            if i+j<2*x/3:
+                BOARD[i][j] = -1
+            if i+j>2*(y+2*x/3):
+                BOARD[i][j] = -1
+            if j-i>2*(2*x/3):
+                BOARD[i][j] = -1
+            if i-j>2*(2*x/3):
                 BOARD[i][j] = -1
     return BOARD
 
 board = board_init(x,y)
+for i in range(2*x):
+    for j in range(2*y):
+        if board[i][j]==1:
+            plt.scatter(j, i, c='black', s=200,marker='h',edgecolors='white')
+#plt.imshow(board, cmap='gray', interpolation='nearest', )
+plt.show()
 
 print(board)
